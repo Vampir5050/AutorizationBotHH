811961d358b17e48003f8dda73360c7f81cc43df
Vampir5050Dev <sanya.afonin33@gmail.com>
2023-05-14 12:22:58 +0300

    первый коммит

---
+++ AutorizationBotHH/Program.cs
@@ -0,0 +1,26 @@
+﻿using AutorizationBotHH;
+using Microsoft.Playwright;
+using System.Net;
+
+var user = new User();
+using var playwright = await Playwright.CreateAsync();
+await using IBrowser browser = await playwright.Firefox.LaunchAsync(
+    new BrowserTypeLaunchOptions
+    {
+        Headless = false
+    });
+BrowserNewContextOptions device = playwright.Devices["Desktop Firefox"];
+await using IBrowserContext context = await browser.NewContextAsync(device);
+IPage page = await context.NewPageAsync();
+await Login();
+async Task Login()
+{
+    await page.GotoAsync("https://hh.ru/login");
+    await page.ClickAsync("button[data-qa ='expand-login-by-password']");
+    await page.TypeAsync("input[data-qa='login-input-username']", user.login);
+    await page.TypeAsync("input[data-qa='login-input-password']", user.password);
+    await page.ClickAsync("button[data-qa ='account-login-submit']");
+}
+
+
+Console.ReadKey();
---
+++ AutorizationBotHH/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutorizationBotHH
+{
+    public class User
+    {
+        public string ?login = Environment.GetEnvironmentVariable("credentinals.Login");
+        public string ?password = Environment.GetEnvironmentVariable("credentinals.Password");
+
+    }
+}
